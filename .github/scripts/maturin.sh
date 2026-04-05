67de6bc4ed5ad35de57f41e335071ab23dc9ab0c
rara64 <60274052+rara64@users.noreply.github.com>
2026-04-05 18:05:08 +0200

    Add maturin script

---
+++ .github/scripts/maturin.sh
@@ -0,0 +1,13 @@
+LINK=$(curl --silent "https://api.github.com/repos/rara64/armv5-homeassistant/actions/artifacts?per_page=1000&page=1" | jq -r '.artifacts[] | select(.name == "maturin") | .archive_download_url' | head -n 1)
+
+curl -L -H "Authorization: token ${{ secrets.WHEELS_TOKEN }}" -o maturin.zip "$LINK" || echo 'MATURIN download failed!'
+7z e maturin.zip -o./maturin -y || echo 'MATURIN extract failed!'
+
+MATURIN_REQUIRED_VER=$(curl -s https://api.github.com/repos/PyO3/maturin/releases/latest | jq -r .tag_name | sed 's/^v//')
+MATURIN_BUILD_VER=$(cat ./maturin/maturin_ver.txt)
+
+if [ "$MATURIN_REQUIRED_VER" = "$MATURIN_BUILD_VER" ]; then
+  exit 0
+else
+  exit 3
+fi
