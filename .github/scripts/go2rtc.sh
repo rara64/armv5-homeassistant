756a7baee8f81564b6cd18ccacc86a615b6c42b8
rara64 <60274052+rara64@users.noreply.github.com>
2026-06-11 12:29:56 +0200

    Add authorization token to GitHub API requests

--- .github/scripts/go2rtc.sh
+++ .github/scripts/go2rtc.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-LINK=$(curl --silent "https://api.github.com/repos/rara64/armv5-homeassistant/actions/artifacts?per_page=1000&page=1" | jq -r '.artifacts[] | select(.name == "go2rtc") | .archive_download_url' | head -n 1)
+LINK=$(curl --silent \
+  -H "Authorization: token ${WHEELS_TOKEN}" \
+  "https://api.github.com/repos/rara64/armv5-homeassistant/actions/artifacts?per_page=1000&page=1" \
+  | jq -r '.artifacts[] | select(.name == "go2rtc") | .archive_download_url' | head -n 1)
 
 curl -L -H "Authorization: token ${WHEELS_TOKEN}" -o go2rtc.zip "$LINK" || echo 'GO2RTC download failed!'
 7z e go2rtc.zip -o./go2rtc -y || echo 'GO2RTC extract failed!'
 
 HOMEASSISTANT_TAG=$1
-GO2RTC=$(curl -s https://raw.githubusercontent.com/home-assistant/core/refs/tags/$HOMEASSISTANT_TAG/Dockerfile | grep -oP '(?<=--from=)ghcr\.io/alexxit/go2rtc[^ ]+')
+GO2RTC=$(curl --silent \
+  -H "Authorization: token ${WHEELS_TOKEN}" \
+  https://raw.githubusercontent.com/home-assistant/core/refs/tags/$HOMEASSISTANT_TAG/Dockerfile \
+  | grep -oP '(?<=--from=)ghcr\.io/alexxit/go2rtc[^ ]+')
 
 GO2RTC_REQUIRED_VER=$(skopeo inspect docker://$GO2RTC | jq -r '.Labels["org.opencontainers.image.version"]')
 GO2RTC_BUILD_VER=$(cat ./go2rtc/go2rtc_ver.txt)
